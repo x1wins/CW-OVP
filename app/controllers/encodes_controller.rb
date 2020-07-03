966bd81a79b8edb6f892aa9b0f31f966f15592a1
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-02 17:58:36 -0700

    rails g scaffold encode log:text started_at:timestamp ended_at:timestamp runtime:float completed:boolean user:references published:boolean

---
+++ app/controllers/encodes_controller.rb
@@ -0,0 +1,74 @@
+class EncodesController < ApplicationController
+  before_action :set_encode, only: [:show, :edit, :update, :destroy]
+
+  # GET /encodes
+  # GET /encodes.json
+  def index
+    @encodes = Encode.all
+  end
+
+  # GET /encodes/1
+  # GET /encodes/1.json
+  def show
+  end
+
+  # GET /encodes/new
+  def new
+    @encode = Encode.new
+  end
+
+  # GET /encodes/1/edit
+  def edit
+  end
+
+  # POST /encodes
+  # POST /encodes.json
+  def create
+    @encode = Encode.new(encode_params)
+
+    respond_to do |format|
+      if @encode.save
+        format.html { redirect_to @encode, notice: 'Encode was successfully created.' }
+        format.json { render :show, status: :created, location: @encode }
+      else
+        format.html { render :new }
+        format.json { render json: @encode.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /encodes/1
+  # PATCH/PUT /encodes/1.json
+  def update
+    respond_to do |format|
+      if @encode.update(encode_params)
+        format.html { redirect_to @encode, notice: 'Encode was successfully updated.' }
+        format.json { render :show, status: :ok, location: @encode }
+      else
+        format.html { render :edit }
+        format.json { render json: @encode.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /encodes/1
+  # DELETE /encodes/1.json
+  def destroy
+    @encode.destroy
+    respond_to do |format|
+      format.html { redirect_to encodes_url, notice: 'Encode was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_encode
+      @encode = Encode.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def encode_params
+      params.require(:encode).permit(:log, :started_at, :ended_at, :runtime, :completed, :user_id, :published)
+    end
+end
---
+++ app/models/encode.rb
@@ -0,0 +1,3 @@
+class Encode < ApplicationRecord
+  belongs_to :user
+end
