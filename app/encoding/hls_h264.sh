c1339c44f5d44f799f8eb96944bbc41213f59577
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-13 01:04:48 -0700

    Added -p in mkdir

--- app/encoding/hls_h264.sh
+++ app/encoding/hls_h264.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "encoding start " $1 $2
-mkdir $1
+mkdir -p $1
 ffmpeg -hide_banner -y -i $2 \
   -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename $1/360p_%03d.ts $1/360p.m3u8 \
   -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename $1/480p_%03d.ts $1/480p.m3u8 \
