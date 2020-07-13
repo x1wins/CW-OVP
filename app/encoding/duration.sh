7a5fc74d0090a471112a14d800bbe0986e1df9b6
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-12 23:28:00 -0700

    Deleted mp4 file extension

--- app/encoding/duration.sh
+++ app/encoding/duration.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Duration
-ffmpeg -i $1.mp4 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//
+ffmpeg -i $1 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//
+
+# Result
+# ffmpeg -i 0000000003.ts 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//
+# 00:00:03.46
