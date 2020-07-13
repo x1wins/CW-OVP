9aa5af0a0a547b90cfd7d6a4317198df16ec1d05
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-13 15:25:32 -0700

    Added default_values published = true, completed = false

--- app/models/encode.rb
+++ app/models/encode.rb
@@ -2,4 +2,9 @@ class Encode < ApplicationRecord
   belongs_to :user
   has_one_attached :file
   validates :file, presence: true, blob: { content_type: :video } # supported options: :image, :audio, :video, :text
+  before_save :default_values
+  def default_values
+    self.published = true
+    self.completed = false
+  end
 end
