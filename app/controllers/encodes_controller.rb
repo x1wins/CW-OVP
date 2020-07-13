c72179873db971c61f1494be3f6e5c50b938773c
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-13 09:23:55 -0700

    Changed logger position

--- app/controllers/encodes_controller.rb
+++ app/controllers/encodes_controller.rb
@@ -32,12 +32,12 @@ class EncodesController < ApplicationController
 
           @encode.file.open do |f|
             temp_file_full_path = f.path
-            Rails.logger.debug "temp file path : #{temp_file_full_path}"
             duration_output = `sh app/encoding/duration.sh #{temp_file_full_path}`
-            Rails.logger.debug "ffmpeg parameter : #{File.basename(f.path)} #{temp_file_full_path}"
             file_path = "hls/#{File.basename(temp_file_full_path, ".*")}"
             file_full_path = "public/#{file_path}"
             encoding_output = `sh app/encoding/hls_h264.sh #{file_full_path} #{temp_file_full_path}`
+            Rails.logger.debug "temp file path : #{temp_file_full_path}"
+            Rails.logger.debug "ffmpeg parameter : #{file_full_path} #{temp_file_full_path}"
             Rails.logger.debug "output : #{duration_output}"
             Rails.logger.debug "encoding_output : #{encoding_output}"
             Rails.logger.debug "full url : #{request.base_url}/#{file_path}/1080p.m3u8"
