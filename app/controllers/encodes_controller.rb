21be9b720b5e61ea9b02b5b93c96695fc2c2559b
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-13 15:27:03 -0700

    Changed strong param to title, merge(user_id: 1)

--- app/controllers/encodes_controller.rb
+++ app/controllers/encodes_controller.rb
@@ -29,15 +29,16 @@ class EncodesController < ApplicationController
     respond_to do |format|
       if @encode.save
         if @encode.file.attached?
-
           @encode.file.open do |f|
+            @encode.update(started_at: Time.now)
             temp_file_full_path = f.path
             duration_output = `sh app/encoding/duration.sh #{temp_file_full_path}`
             file_path = "hls/#{File.basename(temp_file_full_path, ".*")}"
             file_full_path = "public/#{file_path}"
             encoding_output = `sh app/encoding/hls_h264.sh #{file_full_path} #{temp_file_full_path}`
             url = "#{request.base_url}/#{file_path}/1080p.m3u8"
-            @encode.update(url: url)
+            # :log, :started_at, :ended_at, :runtime, :completed
+            @encode.update(log: encoding_output, ended_at: Time.now, runtime: duration_output, completed: true, url: url)
             Rails.logger.debug "temp file path : #{temp_file_full_path}"
             Rails.logger.debug "ffmpeg parameter : #{file_full_path} #{temp_file_full_path}"
             Rails.logger.debug "output : #{duration_output}"
@@ -87,6 +88,6 @@ class EncodesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def encode_params
-      params.require(:encode).permit(:log, :started_at, :ended_at, :runtime, :completed, :user_id, :published, :file)
+      params.require(:encode).permit(:title, :file).merge(user_id: 1) # @current_user.id
     end
 end
