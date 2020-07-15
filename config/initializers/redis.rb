e70dd8f6b088a148b208402a83db59f18392722d
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-15 12:27:07 -0700

    Changed namespace value to "cw_ovp"

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 # config/initializers/redis.rb
 
-$redis = Redis::Namespace.new("tutorial_post", :redis => Redis.new(:host => '127.0.0.1', :port => 7001))
+$redis = Redis::Namespace.new("cw_ovp", :redis => Redis.new(:host => '127.0.0.1', :port => 7001))
 
