fe92c0fbbf6b7debdcf526972b0160c228b9a073
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-16 08:16:18 -0700

    Added session: valid_session

--- spec/controllers/encodes_controller_spec.rb
+++ spec/controllers/encodes_controller_spec.rb
@@ -24,27 +24,29 @@ require 'rails_helper'
 # `rails-controller-testing` gem.
 
 RSpec.describe EncodesController, type: :controller do
+  login_user
 
-  # This should return the minimal set of attributes required to create a valid
-  # Encode. As you add validations to Encode, be sure to
-  # adjust the attributes here as well.
+  before(:all) do
+    @user = FactoryBot.create(:user)
+  end
+
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {title: "hello", user: @user, file: Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.mp4"))}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {title: "", user: @user, file: nil}
   }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # EncodesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  # let(:valid_session) { {} }
 
   describe "GET #index" do
     it "returns a success response" do
       Encode.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      get :index
       expect(response).to be_successful
     end
   end
@@ -52,14 +54,14 @@ RSpec.describe EncodesController, type: :controller do
   describe "GET #show" do
     it "returns a success response" do
       encode = Encode.create! valid_attributes
-      get :show, params: {id: encode.to_param}, session: valid_session
+      get :show, params: {id: encode.to_param}
       expect(response).to be_successful
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
-      get :new, params: {}, session: valid_session
+      get :new, params: {}
       expect(response).to be_successful
     end
   end
@@ -67,7 +69,7 @@ RSpec.describe EncodesController, type: :controller do
   describe "GET #edit" do
     it "returns a success response" do
       encode = Encode.create! valid_attributes
-      get :edit, params: {id: encode.to_param}, session: valid_session
+      get :edit, params: {id: encode.to_param}
       expect(response).to be_successful
     end
   end
@@ -76,19 +78,19 @@ RSpec.describe EncodesController, type: :controller do
     context "with valid params" do
       it "creates a new Encode" do
         expect {
-          post :create, params: {encode: valid_attributes}, session: valid_session
+          post :create, params: {encode: valid_attributes}
         }.to change(Encode, :count).by(1)
       end
 
       it "redirects to the created encode" do
-        post :create, params: {encode: valid_attributes}, session: valid_session
+        post :create, params: {encode: valid_attributes}
         expect(response).to redirect_to(Encode.last)
       end
     end
 
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {encode: invalid_attributes}, session: valid_session
+        post :create, params: {encode: invalid_attributes}
         expect(response).to be_successful
       end
     end
@@ -102,14 +104,14 @@ RSpec.describe EncodesController, type: :controller do
 
       it "updates the requested encode" do
         encode = Encode.create! valid_attributes
-        put :update, params: {id: encode.to_param, encode: new_attributes}, session: valid_session
+        put :update, params: {id: encode.to_param, encode: new_attributes}
         encode.reload
         skip("Add assertions for updated state")
       end
 
       it "redirects to the encode" do
         encode = Encode.create! valid_attributes
-        put :update, params: {id: encode.to_param, encode: valid_attributes}, session: valid_session
+        put :update, params: {id: encode.to_param, encode: valid_attributes}
         expect(response).to redirect_to(encode)
       end
     end
@@ -117,7 +119,7 @@ RSpec.describe EncodesController, type: :controller do
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'edit' template)" do
         encode = Encode.create! valid_attributes
-        put :update, params: {id: encode.to_param, encode: invalid_attributes}, session: valid_session
+        put :update, params: {id: encode.to_param, encode: invalid_attributes}
         expect(response).to be_successful
       end
     end
@@ -127,13 +129,13 @@ RSpec.describe EncodesController, type: :controller do
     it "destroys the requested encode" do
       encode = Encode.create! valid_attributes
       expect {
-        delete :destroy, params: {id: encode.to_param}, session: valid_session
+        delete :destroy, params: {id: encode.to_param}
       }.to change(Encode, :count).by(-1)
     end
 
     it "redirects to the encodes list" do
       encode = Encode.create! valid_attributes
-      delete :destroy, params: {id: encode.to_param}, session: valid_session
+      delete :destroy, params: {id: encode.to_param}
       expect(response).to redirect_to(encodes_url)
     end
   end
