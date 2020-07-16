221f1c82d067bb58b7f82071e22b18db0720e37a
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-16 09:59:14 -0700

    Changed hard coding /hello/ to @title

--- spec/views/encodes/index.html.erb_spec.rb
+++ spec/views/encodes/index.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "encodes/index", type: :view do
   before(:each) do
     @user = FactoryBot.create(:user)
+    @title = "hello"
     assign(:encodes, Kaminari.paginate_array([
       Encode.create!(title: "hello", user: @user, file: Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.mp4")))
     ]).page(1))
@@ -10,6 +11,6 @@ RSpec.describe "encodes/index", type: :view do
 
   it "renders a list of encodes" do
     render
-    expect(rendered).to match /hello/
+    expect(rendered).to match @title
   end
 end
