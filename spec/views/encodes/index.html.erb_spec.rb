ea6e223ecb95e9b0a8823661d2b14e35099bc698
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-16 09:10:24 -0700

    Added Kaminari.paginate_array for index.html.erb_spec

--- spec/views/encodes/index.html.erb_spec.rb
+++ spec/views/encodes/index.html.erb_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "encodes/index", type: :view do
   before(:each) do
-    assign(:encodes, [
-      Encode.create!(),
-      Encode.create!()
-    ])
+    @user = FactoryBot.create(:user)
+    assign(:encodes, Kaminari.paginate_array([
+      Encode.create!(title: "hello", user: @user, file: Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.mp4")))
+    ]).page(1))
   end
 
   it "renders a list of encodes" do
