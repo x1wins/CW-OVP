cf54230f29b9fcf42e8586f06685e41a066b8da8
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-16 09:17:18 -0700

    Added user, Encode.create! param

--- spec/views/encodes/show.html.erb_spec.rb
+++ spec/views/encodes/show.html.erb_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "encodes/show", type: :view do
   before(:each) do
-    @encode = assign(:encode, Encode.create!())
+    @user = FactoryBot.create(:user)
+    @encode = assign(:encode, Encode.create!(title: "hello", user: @user, file: Rack::Test::UploadedFile.new(Rails.root.join("spec/factories/sample.mp4"))))
   end
 
   it "renders attributes in <p>" do
