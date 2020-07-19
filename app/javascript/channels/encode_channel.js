19c0b4405318abca7b6f7b43280c0a2073ce3fb4
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-19 11:01:41 -0700

    Added     console.log("Connected to the room!");

--- app/javascript/channels/encode_channel.js
+++ app/javascript/channels/encode_channel.js
@@ -3,13 +3,17 @@ import consumer from "./consumer"
 consumer.subscriptions.create("EncodeChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("Connected to the room!");
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log("disconnected")
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log("Recieving:")
+    console.log(data.content)
   }
 });
