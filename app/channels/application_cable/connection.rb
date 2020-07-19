3899892155b1ff821428c85cf653b111a57b201e
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-19 00:18:11 -0700

    Added identified_by :current_user https://edgeguides.rubyonrails.org/action_cable_overview.html

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    rescue_from StandardError, with: :report_error
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+    def find_verified_user
+      if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+    def report_error(e)
+      SomeExternalBugtrackingService.notify(e)
+    end
   end
 end
