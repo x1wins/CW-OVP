26845e8e9e0138332a9f0d48a79c7212a5f6bafa
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-30 23:56:45 -0700

    Added thumbnail attach

--- app/models/encode.rb
+++ app/models/encode.rb
@@ -1,6 +1,7 @@
 class Encode < ApplicationRecord
   belongs_to :user
   has_one_attached :file
+  has_one_attached :thumbnail
   validates :title, presence: true
   validates :file, presence: true, blob: { content_type: :video } # supported options: :image, :audio, :video, :text
   scope :published, -> { where(published: true) }
