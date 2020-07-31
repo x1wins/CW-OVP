83cbd623899cda5ada58c2f835c23d1d536bed3f
Chang-Woo Rhee <x1wins@changwoo.net>
2020-07-30 23:07:33 -0700

    Added /thumbnail.png in thumbnail bash script

--- app/encoding/thumbnail.sh
+++ app/encoding/thumbnail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # thumbnail
 # ffmpeg -i SampleVideo_1280x720_5mb.mp4 -vf scale=w=-2:h=360 -ss 00:00:17.000 -vframes 1 out_13.png
-ffmpeg -i $1 -vf scale=w=-2:h=360 -ss $2 -vframes 1 $3.png
+ffmpeg -i $1 -vf scale=w=-2:h=360 -ss $2 -vframes 1 $3/thumbnail.png
