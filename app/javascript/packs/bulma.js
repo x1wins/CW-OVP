f5a879a5caf5736007e7b77a76b9b9246f35edb6
Chang-Woo Rhee <x1wins@changwoo.net>
2020-08-07 16:02:10 -0700

    Changed bulma.js link to require("packs/bulma") in application.js

--- app/javascript/packs/bulma.js
+++ app/javascript/packs/bulma.js
@@ -1,13 +1,14 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
+
+document.addEventListener("turbolinks:load", function() {
     var burger = document.querySelector('.burger');
     var menu = document.querySelector('#'+burger.dataset.target);
     burger.addEventListener('click', function() {
         burger.classList.toggle('is-active');
         menu.classList.toggle('is-active');
     });
-})();
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
