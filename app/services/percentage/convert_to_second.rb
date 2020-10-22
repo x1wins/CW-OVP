033405aab21da503fa79e734a350fdede515c1aa
Chang-Woo <x1wins@naver.com>
2020-10-22 03:15:48 -0700

    Added Percentage::ConvertToSecond

--- app/services/percentage/convert_to_second.rb
+++ app/services/percentage/convert_to_second.rb
@@ -1,3 +1,18 @@
+module Percentage
+  class ConvertToSecond < ApplicationService
+    attr_reader :time
 
-class ConvertToSecond
+    def initialize(time)
+      @time = time
+    end
+
+    def call
+      convert_to_second(@time)
+    end
+
+    private
+      def convert_to_second time
+        (Time.parse(time).to_i - Date.today.to_time.to_i).to_f
+      end
+  end
 end
