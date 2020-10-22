0354db28e62bd85d1d7dddd6f414c42314d8f238
Chang-Woo <x1wins@naver.com>
2020-10-22 03:15:24 -0700

    Added Percentage::Encode

--- app/services/percentage/encode.rb
+++ app/services/percentage/encode.rb
@@ -1,3 +1,29 @@
+module Percentage
+  class Encode < ApplicationService
+    attr_reader :now_time, :total_time
 
-module Encode
+    def initialize(now_time, total_time)
+      @now_time = now_time
+      @total_time = total_time
+    end
+
+    def call
+      encode_half_percentage(@now_time, @total_time)
+    end
+
+    private
+      def encode_half_percentage now_time = nil, total_time = nil
+        encode_percentage(now_time, total_time)/2
+      end
+
+      def encode_percentage now_time = nil, total_time = nil
+        if now_time.nil? or total_time.nil?
+          percentage = 0
+        else
+          percentage = (Percentage::ConvertToSecond.call(now_time) / Percentage::ConvertToSecond.call(total_time) * 100)
+          percentage = 100 if percentage > 100
+        end
+        percentage
+      end
+  end
 end
