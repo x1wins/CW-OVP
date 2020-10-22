de2b58959cd6432bb3bc5ed006a38dbd7d045fe1
Chang-Woo <x1wins@naver.com>
2020-10-22 03:15:07 -0700

    Added Percentage::Cdn

--- app/services/percentage/cdn.rb
+++ app/services/percentage/cdn.rb
@@ -1,3 +1,23 @@
+module Percentage
+  class Cdn < ApplicationService
+    attr_reader :total_file_count, :file_number
 
-module Cdn
+    def initialize(total_file_count, file_number)
+      @total_file_count = total_file_count
+      @file_number = file_number
+    end
+
+    def call
+      cdn_cp_half_percentage @total_file_count, @file_number
+    end
+
+    private
+      def cdn_cp_half_percentage total_file_count, file_number
+        cdn_cp_percentage(total_file_count, file_number)/2
+      end
+
+      def cdn_cp_percentage total_file_count, file_number
+        ((total_file_count - file_number).to_f / total_file_count.to_f * 100)
+      end
+  end
 end
