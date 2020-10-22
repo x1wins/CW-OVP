b440742f80a1ceb1ff6e652435f1b1ee4f35f117
Chang-Woo <x1wins@naver.com>
2020-10-22 01:55:46 -0700

    Changed old path, url code to refactored module code

--- app/workers/encode_worker.rb
+++ app/workers/encode_worker.rb
@@ -7,7 +7,7 @@ class EncodeWorker
     if encode.file.attached?
       encode.file.open do |f|
         uploaded_file_path = f.path
-        hls_local_full_path = Storage::Path.hls
+        hls_local_full_path = Storage::Path::Local::Hls.call(encode)
         runtime = `sh app/encoding/runtime.sh #{uploaded_file_path}`
         mkdir_cmd = `sh app/encoding/mkdir.sh #{hls_local_full_path}`
         encoding_cmd = "sh app/encoding/hls_h264.sh #{hls_local_full_path} #{uploaded_file_path}"
@@ -32,8 +32,8 @@ class EncodeWorker
         playlist_cp_cmd = `cp app/encoding/playlist.m3u8 #{hls_local_full_path}/`
 
         cdn_bucket = ENV['CDN_BUCKET']
-        hls_relative_path = Storage::Path::HlsRelativePath.call(encode)
-        hls_local_full_path = Storage::Path::HlsLocalFullPath.call(encode)
+        hls_relative_path = Storage::Url::Relative::Hls.call(encode)
+        hls_local_full_path = Storage::Path::Local::Hls.call(encode)
         move_hls_to_cdn_cmd = "sh app/encoding/mv.sh #{cdn_bucket} #{hls_relative_path} #{hls_local_full_path}"
         Sidekiq.logger.info "move_hls_to_cdn_cmd : #{move_hls_to_cdn_cmd}"
         total_file_count = 0
@@ -55,16 +55,16 @@ class EncodeWorker
           end
         end
 
-        video_url = encode.video_url base_url
-        encode.update(log: log, ended_at: Time.now, completed: true, url: video_url)
-        encode.assets.create(format: 'video', url: video_url)
+        hls_url = Storage::Url::Full::Hls.call(encode, base_url)
+        encode.update(log: log, ended_at: Time.now, completed: true, url: hls_url)
+        encode.assets.create(format: 'video', url: hls_url)
         encode.send_message "Completed Move Local File To AWS S3", log, "100%"
 
         Sidekiq.logger.debug "move_hls_to_cdn_cmd : #{move_hls_to_cdn_cmd}"
         Sidekiq.logger.debug "ffmpeg parameter : #{hls_local_full_path} #{uploaded_file_path}"
         Sidekiq.logger.debug "output : #{runtime}"
         Sidekiq.logger.debug "log : #{log}"
-        Sidekiq.logger.debug "video_url : #{video_url}"
+        Sidekiq.logger.debug "video_url : #{hls_url}"
       end
     end
   end
--- app/workers/thumbnail_worker.rb
+++ app/workers/thumbnail_worker.rb
@@ -7,7 +7,7 @@ class ThumbnailWorker
     if encode.file.attached?
       encode.file.open do |f|
         uploaded_file_path = f.path
-        thumbnail_local_full_path = encode.thumbnail_local_full_path
+        thumbnail_local_full_path = Storage::Path::Local::Thumbnail.call(encode)
         runtime = `sh app/encoding/runtime.sh #{uploaded_file_path}`
         mkdir_cmd = `sh app/encoding/mkdir.sh #{thumbnail_local_full_path}`
         log = ""
@@ -18,7 +18,7 @@ class ThumbnailWorker
           thumbnail_filename = encode.thumbnail_filename ss, i
           thumbnail_file_full_path = "#{thumbnail_local_full_path}/#{thumbnail_filename}"
           thumbnail_cmd = `sh app/encoding/thumbnail.sh #{uploaded_file_path} #{ss} #{thumbnail_file_full_path}`
-          thumbnail_url = encode.thumbnail_url base_url, thumbnail_filename
+          thumbnail_url = Storage::Url::Full::Thumbnail.call(encode, base_url, thumbnail_filename)
           encode.assets.create(format: 'image', url: thumbnail_url)
           encode.thumbnails.attach(io: File.open(thumbnail_file_full_path), filename: thumbnail_filename, content_type: "image/png")
           thumbnail_rails_url = Rails.application.routes.url_helpers.rails_blob_path(encode.thumbnails.last, disposition: "attachment", only_path: true)
@@ -30,8 +30,8 @@ class ThumbnailWorker
       end
 
       cdn_bucket = ENV['CDN_BUCKET']
-      thumbnail_relative_path = encode.thumbnail_relative_path
-      thumbnail_local_full_path = encode.thumbnail_local_full_path
+      thumbnail_relative_path = Storage::Url::Relative::Thumbnail.call(encode)
+      thumbnail_local_full_path = Storage::Path::Local::Thumbnail.call(encode)
       move_thumbnail_to_cdn_cmd = `sh app/encoding/mv.sh #{cdn_bucket} #{thumbnail_relative_path} #{thumbnail_local_full_path}`
       Sidekiq.logger.debug "move_thumbnail_to_cdn_cmd : #{move_thumbnail_to_cdn_cmd}"
       message = ""
