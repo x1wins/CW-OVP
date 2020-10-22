2e1a325982a5a0d20cbb4f6c98a9b01981e8f00a
Chang-Woo <x1wins@naver.com>
2020-10-22 03:15:59 -0700

    Added Percentage::ToString

--- app/services/percentage/to_string.rb
+++ app/services/percentage/to_string.rb
@@ -1,3 +1,22 @@
+module Percentage
+  class ToString < ApplicationService
+    attr_reader :percentage
 
-class ToString
+    def initialize(percentage)
+      @percentage = percentage
+    end
+
+    def call
+      percentage_to_s(@percentage)
+    end
+
+    private
+      def percentage_to_s percentage
+        rount_off_two_decial_place(percentage).to_s + "%"
+      end
+
+      def rount_off_two_decial_place percentage
+        '%.2f' % percentage
+      end
+  end
 end
