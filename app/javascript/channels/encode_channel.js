8627454cd6963009909e0360f81640b84989f327
Chang-Woo <x1wins@naver.com>
2020-10-22 17:41:52 -0700

    Added onEncodeIndex, onEncodeShow
    (cherry picked from commit de1b0fbf7ce25bb119e9dcc9454b6243a22ad601)

--- app/javascript/channels/encode_channel.js
+++ app/javascript/channels/encode_channel.js
@@ -15,111 +15,15 @@ consumer.subscriptions.create("EncodeChannel", {
     // Called when there's incoming data on the websocket for this channel
     console.log("Recieving:")
     console.log("encode_id: "+data.encode_id + " content:" + data.content + " percentage:" + data.percentage + "thumbnail_url:"+ data.thumbnail_url)
-    var encodes_table = document.getElementById("encodes");
-    if(encodes_table){
-      console.log("encodes_table exist")
-      var tr = document.querySelectorAll("[id='encode_id']");
-      var count = 0;
-      console.log("tr.length : " + tr.length)
-      for(var i = 0; i < tr.length; i++){
-        var data_encode_id = tr[i].getAttribute("data-encode-id")
-        if(data_encode_id == data.encode_id){
-          count ++;
-        }
-      }
-      if(count == 0){
-        var encode = data.encode;
-        var filename = data.filename;
-        var row = encodes_table.insertRow(1);
-        row.setAttribute("id","encode_id");
-        row.setAttribute("data-encode-id",encode.id);
-        var id_cell = document.createElement('th');
-        row.appendChild(id_cell);
-        var title_cell = row.insertCell(1);
-        var filename_cell = row.insertCell(2);
-        var runtime_cell = row.insertCell(3);
-        var url_cell = row.insertCell(4);
-        var craeted_at_cell = row.insertCell(5);
-        var completed_cell = row.insertCell(6);
-        completed_cell.setAttribute("id","status_"+encode.id);
-        var del_cell = row.insertCell(7);
-        id_cell.innerHTML = encode.id;
-        title_cell.innerHTML = encode.title;
-        filename_cell.innerHTML = filename;
-        runtime_cell.innerHTML = encode.runtime;
-        url_cell.innerHTML = encode.url;
-        craeted_at_cell.innerHTML = encode.created_at;
-        completed_cell.innerHTML = encode.completed;
-        del_cell.innerHTML = "";
-      }
 
-      var status = document.getElementById("status_"+data.encode_id);
-      if(status){
-        var encode = data.encode;
-        var tr = document.querySelectorAll("[id='encode_id']");
-        var found_index = 0;
-        for(var i = 0; i < tr.length; i++){
-          var data_encode_id = tr[i].getAttribute("data-encode-id")
-          if(data_encode_id == encode.id){
-            found_index = i;
-          }
-        }
-        var tds = tr[found_index].getElementsByTagName("td")
-        if(data.percentage){
-          status.innerHTML = data.percentage
-        }
-        if(data.percentage == '100%'){
-          var filename = data.filename;
-          tds[1].innerHTML = filename;
-          tds[2].innerHTML = encode.runtime;
-          tds[3].innerHTML = encode.url;
-          tds[5].innerHTML = encode.completed;
-        }
-        if(data.thumbnail_url){
-          addThumbnailInIndex(tds[6], data.thumbnail_url)
-        }
+    var controller_name = document.getElementById("controller_name").value;
+    var action_name = document.getElementById("action_name").value;
+    if(controller_name == "encodes"){
+      if(action_name == "index"){
+        onEncodeIndex(data);
+      }else if(action_name == "show"){
+        onEncodeShow(data);
       }
-      return
-    }
-
-    var logs_table = document.getElementById("logs");
-    var encode_id = document.getElementById("encode_id");
-    var received_encode_id = data.encode_id;
-    if(logs_table && encode_id && (encode_id.value == received_encode_id)){
-      console.log("data.type:"+data.type);
-      if(data.type == 'cp_thumbnail'){
-        console.log("asset.url " + data.content);
-        var thumbnail = document.getElementById("thumbnail");
-        thumbnail.innerHTML = data.content;
-        return
-      }
-      addThumbnailInShow(data.thumbnail_url);
-      encode = data.encode;
-      var runtime = document.getElementById("runtime");
-      runtime.innerHTML = encode.runtime;
-      var progress = document.getElementById("progress");
-      if(data.percentage){
-        var progress_value = data.percentage.replace('%', '');
-        progress.setAttribute("value", progress_value);
-        var percentage = document.getElementById("percentage");
-        percentage.innerHTML = data.percentage;
-      }
-      var content = data.content;
-      var row = logs_table.insertRow(logs_table.size);
-      var log_cell = row.insertCell(0);
-      log_cell.innerHTML = content;
-
-      if(encode.completed == true){
-        console.log("completed");
-        var ended_at = document.getElementById("ended_at");
-        ended_at.innerHTML = encode.ended_at;
-        var url = document.getElementById("url");
-        url.innerHTML = encode.url;
-        var completed = document.getElementById("completed");
-        completed.innerHTML = encode.completed;
-      }
-      scrollingLogContainerToBottom();
-      return
     }
   }
 });
@@ -154,6 +58,118 @@ function addThumbnailInIndex(td, thumbnail_url){
     }
   }
 }
+
+function onEncodeIndex(data){
+  var encodes_table = document.getElementById("encodes");
+  if(encodes_table){
+    console.log("encodes_table exist")
+    var tr = document.querySelectorAll("[id='encode_id']");
+    var count = 0;
+    console.log("tr.length : " + tr.length)
+    for(var i = 0; i < tr.length; i++){
+      var data_encode_id = tr[i].getAttribute("data-encode-id")
+      if(data_encode_id == data.encode_id){
+        count ++;
+      }
+    }
+    if(count == 0){
+      var encode = data.encode;
+      var filename = data.filename;
+      var row = encodes_table.insertRow(1);
+      row.setAttribute("id","encode_id");
+      row.setAttribute("data-encode-id",encode.id);
+      var id_cell = document.createElement('th');
+      row.appendChild(id_cell);
+      var title_cell = row.insertCell(1);
+      var filename_cell = row.insertCell(2);
+      var runtime_cell = row.insertCell(3);
+      var url_cell = row.insertCell(4);
+      var craeted_at_cell = row.insertCell(5);
+      var completed_cell = row.insertCell(6);
+      completed_cell.setAttribute("id","status_"+encode.id);
+      var del_cell = row.insertCell(7);
+      id_cell.innerHTML = encode.id;
+      title_cell.innerHTML = encode.title;
+      filename_cell.innerHTML = filename;
+      runtime_cell.innerHTML = encode.runtime;
+      url_cell.innerHTML = encode.url;
+      craeted_at_cell.innerHTML = encode.created_at;
+      completed_cell.innerHTML = encode.completed;
+      del_cell.innerHTML = "";
+    }
+
+    var status = document.getElementById("status_"+data.encode_id);
+    if(status){
+      var encode = data.encode;
+      var tr = document.querySelectorAll("[id='encode_id']");
+      var found_index = 0;
+      for(var i = 0; i < tr.length; i++){
+        var data_encode_id = tr[i].getAttribute("data-encode-id")
+        if(data_encode_id == encode.id){
+          found_index = i;
+        }
+      }
+      var tds = tr[found_index].getElementsByTagName("td")
+      if(data.percentage){
+        status.innerHTML = data.percentage
+      }
+      if(data.percentage == '100%'){
+        var filename = data.filename;
+        tds[1].innerHTML = filename;
+        tds[2].innerHTML = encode.runtime;
+        tds[3].innerHTML = encode.url;
+        tds[5].innerHTML = encode.completed;
+      }
+      if(data.thumbnail_url){
+        addThumbnailInIndex(tds[6], data.thumbnail_url)
+      }
+    }
+    return
+  }
+}
+
+function onEncodeShow(data){
+  var logs_table = document.getElementById("logs");
+  var encode_id = document.getElementById("encode_id");
+  var received_encode_id = data.encode_id;
+  if(logs_table && encode_id && (encode_id.value == received_encode_id)){
+    console.log("data.type:"+data.type);
+    if(data.type == 'cp_thumbnail'){
+      console.log("asset.url " + data.content);
+      var thumbnail = document.getElementById("thumbnail");
+      thumbnail.innerHTML = data.content;
+      return
+    }
+    addThumbnailInShow(data.thumbnail_url);
+    var encode = data.encode;
+    var runtime = document.getElementById("runtime");
+    runtime.innerHTML = encode.runtime;
+    var progress = document.getElementById("progress");
+    if(data.percentage){
+      var progress_value = data.percentage.replace('%', '');
+      progress.setAttribute("value", progress_value);
+      var percentage = document.getElementById("percentage");
+      percentage.innerHTML = data.percentage;
+    }
+    var content = data.content;
+    var row = logs_table.insertRow(logs_table.size);
+    var log_cell = row.insertCell(0);
+    log_cell.innerHTML = content;
+
+    if(encode.completed == true){
+      console.log("completed");
+      var ended_at = document.getElementById("ended_at");
+      ended_at.innerHTML = encode.ended_at;
+      var url = document.getElementById("url");
+      url.innerHTML = encode.url;
+      var completed = document.getElementById("completed");
+      completed.innerHTML = encode.completed;
+    }
+    scrollingLogContainerToBottom();
+    return
+  }
+}
+
 
 document.addEventListener("turbolinks:load", function() {
   scrollingLogContainerToBottom();
