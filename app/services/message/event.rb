29fe430053dd6bf41bfad959aee1463efb86e34f
Chang-Woo <x1wins@naver.com>
2020-10-22 17:20:02 -0700

    Added Message::Event
    (cherry picked from commit cb7966f2029f1bcc5f40088fea9654222d6f4cba)

---
+++ app/services/message/event.rb
@@ -0,0 +1,12 @@
+module Message
+  module Event
+    RETURNS = [
+        HLS_CDN_URL = "HLS_CDN_URL",
+        THUMBNAIL_CDN_URL = "THUMBNAIL_CDN_URL",
+        COMPLETED = "COMPLETED",
+        PROGRESS = "PROGRESS 11",
+        LOG = "LOG",
+        THUMBNAIL_RAILS_URL = "THUMBNAIL_RAILS_URL",
+    ]
+  end
+end
