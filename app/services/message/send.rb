50d1bd1f55f97ed2a3ac4371a6a5336772ea7730
Chang-Woo <x1wins@naver.com>
2020-10-24 02:29:26 -0700

    Deleted @message = @message.to_s+"\n" if @event == Message::Event::LOG

--- app/services/message/send.rb
+++ app/services/message/send.rb
@@ -6,9 +6,7 @@ module Message
     end
 
     def call
-      @message = @message.to_s+"\n" if @event == Message::Event::LOG
       ActionCable.server.broadcast "encode_channel", event: @event, body: @body
-      Rails.logger.info "Send @message: #{@message}"
     end
   end
 end
