7a90308587ae631d0b6d417e732bf3f2da089e69
Chang-Woo <x1wins@naver.com>
2020-10-25 13:18:04 -0700

    Changed encode position to bottom

--- app/services/message/body.rb
+++ app/services/message/body.rb
@@ -2,12 +2,12 @@ module Message
   class Body
     attr_reader :encode, :percentage, :log, :filename, :thumbnail_url
     def initialize(encode, percentage, log, filename, thumbnail_url)
-      @encode = encode
       @percentage = percentage
       @log = log
       @log << "\n" if log.present?
       @filename = filename
       @thumbnail_url = thumbnail_url
+      @encode = encode
     end
   end
 end
