4696ca4c65872c6d748091142f0b129afd83f6a9
Chang-Woo <x1wins@naver.com>
2020-11-12 15:29:03 -0800

    Changed retry: 3 to retry: false

--- app/workers/encode_worker.rb
+++ app/workers/encode_worker.rb
@@ -1,6 +1,6 @@
 class EncodeWorker
   include Sidekiq::Worker
-  sidekiq_options retry: 3, backtrace: true
+  sidekiq_options retry: false, backtrace: true
 
   def perform(encode_id)
     encode = Encode.find(encode_id)
