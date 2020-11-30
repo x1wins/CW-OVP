1467a5ed6e16d1e0a46237ec51730d5868a7be26
Chang-Woo <x1wins@naver.com>
2020-11-30 09:34:17 -0800

    Deleted delete in encodes_controller

--- app/controllers/encodes_controller.rb
+++ app/controllers/encodes_controller.rb
@@ -39,16 +39,6 @@ class EncodesController < ApplicationController
     end
   end
 
-  # DELETE /encodes/1
-  # DELETE /encodes/1.json
-  def destroy
-    @encode.update(published: false)
-    respond_to do |format|
-      format.html { redirect_to encodes_url, notice: 'Encode was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_encode
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   devise_for :users
-  resources :encodes, :except => [:edit, :update]
+  resources :encodes, :except => [:edit, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => "encodes#index"
   authenticate :user do
