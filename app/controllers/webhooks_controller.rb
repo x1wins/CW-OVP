52ba95a31a68d04218e7aad9881a96bb1985cd1e
Chang-Woo <x1wins@naver.com>
2020-12-07 19:24:52 -0800

    Run docker-compose run --no-deps web bundle exec rails g scaffold webhook url:string api_key:string  user_id:integer method:string is_active:boolean

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,74 @@
+class WebhooksController < ApplicationController
+  before_action :set_webhook, only: [:show, :edit, :update, :destroy]
+
+  # GET /webhooks
+  # GET /webhooks.json
+  def index
+    @webhooks = Webhook.all
+  end
+
+  # GET /webhooks/1
+  # GET /webhooks/1.json
+  def show
+  end
+
+  # GET /webhooks/new
+  def new
+    @webhook = Webhook.new
+  end
+
+  # GET /webhooks/1/edit
+  def edit
+  end
+
+  # POST /webhooks
+  # POST /webhooks.json
+  def create
+    @webhook = Webhook.new(webhook_params)
+
+    respond_to do |format|
+      if @webhook.save
+        format.html { redirect_to @webhook, notice: 'Webhook was successfully created.' }
+        format.json { render :show, status: :created, location: @webhook }
+      else
+        format.html { render :new }
+        format.json { render json: @webhook.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /webhooks/1
+  # PATCH/PUT /webhooks/1.json
+  def update
+    respond_to do |format|
+      if @webhook.update(webhook_params)
+        format.html { redirect_to @webhook, notice: 'Webhook was successfully updated.' }
+        format.json { render :show, status: :ok, location: @webhook }
+      else
+        format.html { render :edit }
+        format.json { render json: @webhook.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /webhooks/1
+  # DELETE /webhooks/1.json
+  def destroy
+    @webhook.destroy
+    respond_to do |format|
+      format.html { redirect_to webhooks_url, notice: 'Webhook was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_webhook
+      @webhook = Webhook.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def webhook_params
+      params.require(:webhook).permit(:url, :api_key, :user_id, :method, :is_active)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_193232) do
+ActiveRecord::Schema.define(version: 2020_12_08_032420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(version: 2020_12_07_193232) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "webhooks", force: :cascade do |t|
+    t.string "url"
+    t.string "api_key"
+    t.integer "user_id"
+    t.string "method"
+    t.boolean "is_active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
