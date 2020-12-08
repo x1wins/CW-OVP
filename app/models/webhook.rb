5762ad26cfae0db1e738e8f0ceed7a6ef6f5917b
Chang-Woo <x1wins@naver.com>
2020-12-07 23:46:10 -0800

    Added method

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,3 +1,7 @@
 class Webhook < ApplicationRecord
+  validates :url, presence: true
+  validates :api_key, presence: true
+  validates :method, presence: true
   scope :by_date, -> { order('id DESC') }
+  enum methods: { post: "post", put: "put", get: "get"}
 end
