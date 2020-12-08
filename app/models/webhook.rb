558bdae5c241bb59f7e1a25aa9d0cbba8e534a1f
Chang-Woo <x1wins@naver.com>
2020-12-07 23:53:25 -0800

    Added enum valid for select box - https://stackoverflow.com/questions/8146965/how-do-i-specify-and-validate-an-enum-in-rails

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,7 +1,8 @@
 class Webhook < ApplicationRecord
   validates :url, presence: true
   validates :api_key, presence: true
-  validates :method, presence: true
+  METHOD_OPTIONS = %w(post put get)
+  validates :method, :inclusion => {:in => METHOD_OPTIONS}
   scope :by_date, -> { order('id DESC') }
   enum methods: { post: "post", put: "put", get: "get"}
 end
