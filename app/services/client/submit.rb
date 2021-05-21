6fb20d9cf2c1603161bd6af0ad4695d5b9a1b0a0
Chang-Woo <x1wins@naver.com>
2021-05-20 19:11:33 -0700

    Changed encode model to callback_id, callback_value, callback_format

--- app/services/client/submit.rb
+++ app/services/client/submit.rb
@@ -1,24 +1,27 @@
 module Client
   class Submit  < ApplicationService
-    def initialize(webhook, encode)
+    def initialize(webhook, callback_id, callback_value, callback_format)
       @webhook = webhook
-      @encode = encode
+      @callback_id = callback_id
+      @callback_value = callback_value
+      @callback_format = callback_format
     end
 
     def call
       begin
         url = @webhook.url
         api_key = @webhook.api_key
-        encode_id = @encode.id
-        callback_id = @encode.callback_id
+        callback_id = @callback_id
+        callback_value = @callback_value
+        callback_format = @callback_format
         headers = {}
         uri = URI.parse(url)
         http = Net::HTTP.new(uri.host, uri.port)
-        response = http.post(uri.path, "encode_id=#{encode_id}&callback_id=#{callback_id}&api_key=#{api_key}", headers)
+        response = http.post(uri.path, "callback_id=#{callback_id}&callback_value=#{callback_value}&callback_format=#{callback_format}&api_key=#{api_key}", headers)
       rescue => e
-        logger.info e
+        Sidekiq.logger.info e
       ensure
-        logger.info response
+        Sidekiq.logger.info response
       end
     end
   end
