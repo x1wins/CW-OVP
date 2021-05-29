8c06812dc9e2823313744b9574ebe9e038c0ab9d
Chang-Woo <x1wins@naver.com>
2021-05-29 11:10:42 -0700

    Added Rails.application.routes.default_url_options[:protocol] = 'https'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:protocol] = 'https'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/initializers/force_ssl.rb
+++
@@ -1,3 +0,0 @@
-if Rails.application.config.force_ssl
-  Rails.application.routes.default_url_options[:protocol] = 'https'
-end
