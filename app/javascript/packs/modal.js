8421b9c1de80baf5f55411b7f25f25017bdc97b9
Chang-Woo <x1wins@naver.com>
2021-06-05 23:12:38 -0700

    Fixed disposal, create element for video

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,5 +1,16 @@
 document.addEventListener("turbolinks:load", function() {
     console.log("turbolinks:load modal");
+    var oldPlayer = document.getElementById('my-player');
+    if(oldPlayer){
+        videojs(oldPlayer).dispose();
+    }
+    var videoCntent = document.getElementById('video-content');
+    if(videoCntent){
+        v = buildVideo();
+        videoCntent.append(v);
+        var player = videojs(v);
+    }
+
     const questionBlock = document.querySelector('a#open-modal');
     if(questionBlock == null){
         return;
@@ -19,3 +30,21 @@ document.addEventListener("turbolinks:load", function() {
         });
     });
 });
+
+function buildVideo(){
+    var obj, source;
+    obj = document.createElement('video');
+    obj.setAttribute('id', 'my-player');
+    obj.setAttribute('class', 'video-js');
+    obj.setAttribute('width', '640');
+    obj.setAttribute('data-height', '264');
+    obj.setAttribute('controls', ' ');
+    obj.setAttribute('poster', '//vjs.zencdn.net/v/oceans.png');
+    obj.setAttribute('preload', 'auto');
+    obj.setAttribute('data-setup', '{}');
+    source = document.createElement('source');
+    source.setAttribute('type', 'video/mp4');
+    source.setAttribute('src', '//vjs.zencdn.net/v/oceans.mp4');
+    obj.append(source);
+    return obj;
+}
